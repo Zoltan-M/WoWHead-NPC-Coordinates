bfb237736a45c20aa576b406eb0704e7e5ec94b4
Zoltán Molnár <weper95@gmail.com>
2020-05-15 18:58:46 +0200

    Add new project and add UI
    - Setup project for MVVM pattern
    - Create UI for the project (only the UI elements are added, the code for the UI elements will be added later)

---
+++ WoWHead-NPC-Coordinates/Views/CoordsControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WoWHead_NPC_Coordinates.Views
+{
+    /// <summary>
+    /// Interaction logic for CoordsControl.xaml
+    /// </summary>
+    public partial class CoordsControl : UserControl
+    {
+        public CoordsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
